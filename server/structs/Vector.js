3af856b494f8bb6fbbeba0ead4b4c12ef458baca
= <meeta.36@hotmail.com>
2022-08-03 08:34:10 -0400

    enahncement: make canvas font bold

--- server/structs/Vector.js
+++ server/structs/Vector.js
@@ -14,16 +14,16 @@ class Vector {
         return new Vector(this.x - vector.x, this.y - vector.y);
     }
 
-    scale(scalar) {
+    /*scale(scalar) {
         return new Vector(this.x * scalar, this.y * scalar);
-    }
+    }*/
 
     distance(vector) {
         const subtracted = this.subtract(vector);
         return Math.sqrt(Math.pow(subtracted.x, 2) + Math.pow(subtracted.y, 2)); // math.pow(a, b) is faster than a ** b but makes the code less readable
     }
 
-    dot(vector) {
+    /*dot(vector) {
         return this.x * vector.x + this.y * vector.y;
     }
 
@@ -45,21 +45,21 @@ class Vector {
     }
 
     get mag() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        return Math.hypot(this.x, this.y);
     }
 
     get dir() {
         return Math.atan2(this.y, this.x); // for Math.atan2 it actually takes the y param first for some reason
     }
 
-    get unitVector() {
+    get normalize() {
         const mag = this.mag;
 
         const x = this.x / mag || 0;
         const y = this.y / mag || 0;
 
         return new Vector(x, y);
-    }
+    }*/
 }
 
 module.exports = Vector;
